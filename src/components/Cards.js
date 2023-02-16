47a95583d5550f38f217070917c897f96e03514b
Oluwashina Lasisi <lasisioluwashina@gmail.com>
2023-02-16 11:07:51 +0100

    cybadot  task

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import Users from "../data.js";
+import Users from "../data.js";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteUser } from "../features/user/userSlice";
 import { FaRegTimesCircle } from "react-icons/fa";
@@ -10,13 +10,13 @@ export const Card = () => {
   const users = useSelector((state) => state.user.user);
   console.log(users);
   //search bar
-  const [filteredList, setFilteredList] = useState(users);
+  const [filteredList, setFilteredList] = useState(Users);
   const filterBySearch = (e) => {
     e.preventDefault();
     //accessing input value
     const query = e.target.value;
     //creating copy of users
-    var updatedUsers = [...users];
+    var updatedUsers = [...Users];
     //including all items that include the search query
     updatedUsers = updatedUsers.filter((user, index) => {
       return (
@@ -36,6 +36,10 @@ export const Card = () => {
     //   });
   };
   console.log(filteredList);
+  // const deleteUser1 = filteredList.filter((user, id) =>
+  //   user.splice(user.id, 2)
+  // );
+ 
   //initializing our useDispatch
   const dispatch = useDispatch();
 
@@ -53,7 +57,7 @@ export const Card = () => {
         className="search-box"
       />
       <div className="cards_container">
-        {users.map(
+        {filteredList.map(
           (
             user,
             index //user is the payload
